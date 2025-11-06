15ef1839459628601f9b6b440bcea840828c0c97
Mateus Lage <mmamedel@gmail.com>
2025-11-06 18:31:00 -0300

    refactor: update Prisma client import path

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/edge';
+import { PrismaClient } from '../generated/prisma';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient().$extends(withAccelerate());
